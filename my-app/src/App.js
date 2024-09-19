9ed5c04c51e2f5454794d415fc314f730f3cb129
Florian Hass <f91hama@gmail.com>
2024-09-19 19:26:58 +0000

    Add LifeCyclesCDM, and LifeCyclesCDMChild.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
 import RenderingList from "./components/RenderList";
+import LifeCyclesCDM from "./components/LifeCyclesCDM";
 
 function App() {
   return <div className="App">
-      <RenderingList />
+      <LifeCyclesCDM />
   </div>;
 }
 
