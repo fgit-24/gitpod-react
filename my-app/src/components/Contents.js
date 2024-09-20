b58c9b83861a1bb846e75f4125dcb9bdf4c0f58e
Florian Hass <f91hama@gmail.com>
2024-09-20 18:27:24 +0000

    Setup Contents.js

---
+++ my-app/src/components/Contents.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class Contents extends Component {
+    constructor(){
+        super()
+            this.state = {
+                posts: []
+            }
+    }
+  render() {
+    return (
+      <div>Contents</div>
+    )
+  }
+}
+
+export default Contents
