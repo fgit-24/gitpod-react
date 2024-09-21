65978e45b490c9c6e17dcd2220702686276050a5
Florian Hass <f91hama@gmail.com>
2024-09-21 06:46:35 +0000

    UseState hook lecture

--- my-app/src/components/ControlledFormHooks.js
+++ my-app/src/components/ControlledFormHooks.js
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-function ControlledFormHooks() {
+function ControlledFormHooks() {    
+    const [name, setName] = useState('');
+    const [category, setCategory] = useState('website');
+    const [comments, setComments] = useState('')
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(name, category, comments)
+    }
+
+
   return (
-    <div>It works!</div>
-  )
+    <div>
+        <h2>Please fill out the form below:</h2>
+        <form onSubmit={this.handleSubmit}>
+            <div>
+                <label htmlFor="id-name">Your Name:</label>
+                <input
+                    onChange={
+                        (e) => setName(e.target.value)}
+                    id="id-name"
+                    name="name"
+                    type="text"
+                />
+            </div>
+            <div>
+                <label htmlFor="id-category">Query category:</label>
+                <select
+                    id="id-category"
+                    name="category"
+                    onChange={
+                        (e) => setCategory(e.target.value)}
+                >
+                    <option value="website">Website issue</option>
+                    <option value="order">Order issue</option>
+                    <option value="general">General inquiry</option>
+                </select>
+            </div>
+            <div>
+                <label htmlFor="id-comments">Comments:</label>
+                <textarea
+                    id="id-comments"
+                    name="comments"
+                    onChange={
+                        (e) => setComments(e.target.value)}
+                />
+            </div>
+            <input type="submit" value="Submit" />
+        </form>
+    </div>
+)
 }
 
 export default ControlledFormHooks
